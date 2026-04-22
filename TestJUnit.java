4c465e0de59145bc15e2fc73621d1d2f5eeddcaa
Ariel Penaloza <Ariel Penaloza@STFU>
2026-04-21 22:05:09 -0700

    Build Path for the library JUnit 6

---
+++ TestJUnit.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestJUnit {
+
+	@Test
+	public void testCount8_1() {
+		assertEquals(4, TestingRecursion.count8(8818));
+	}
+
+}
--- TestingRecursion.java
+++ TestingRecursion.java
@@ -3,7 +3,32 @@
 //Testing Recursion
 
 public class TestingRecursion {
+	public static int count8(int n) {
+		
+		if(n < 10)
+			if(n == 8) 
+				return 1;
+			else
+				return 0;
+		
+		int onesPlace = n % 10;
+		n /= 10;
+		int tensPlace = n %10;
+		
+		if(onesPlace == 8)
+			if(tensPlace == 8)
+				return 2 + count8(n);
+			else 
+				return 1 + count8(n);
+		else 
+			return count8(n);
+	}//end count8
+	
+	public static void main(String [] arg) {
+		System.out.println(count8(8818));
 	
 	
+	
+	}
 	
 }
